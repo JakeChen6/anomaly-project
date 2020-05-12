708df63496927367a086ddb50ea1a8b233e59200
Zhishe Chen <33152781+JakeChen6@users.noreply.github.com>
2020-05-12 10:28:59 +0800

    Create main.py

---
+++ 52_week_high/plot/main.py
@@ -0,0 +1,193 @@
+''' Create a simple strategy performance dashboard.
+
+Choose stocks to compare in the drop down widgets, and make selections
+on the plots to update the summary and histograms accordingly.
+
+.. note::
+    Running this example requires downloading sample data. See
+    the included `README`_ for more information.
+
+Use the ``bokeh serve`` command to run the example by executing:
+
+    bokeh serve stocks
+
+at your command prompt. Then navigate to the URL
+
+    http://localhost:5006/stocks
+
+.. _README: https://github.com/bokeh/bokeh/blob/master/examples/app/stocks/README.md
+
+'''
+from functools import lru_cache
+import pickle as pk
+
+import numpy as np
+import pandas as pd
+from scipy import stats as sci_stats
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, PreText, Select
+from bokeh.plotting import figure
+
+DATA_DIR = '/Users/zhishe/myProjects/anomaly/anomalies/3-52-Week High/results'
+
+with open(DATA_DIR + '/monthly_returns.pkl', 'rb') as f:
+    MONTHLY_RETS = pk.load(f)
+
+LOOK_BACK = ['6']
+HOLDING = ['6']
+
+
+@lru_cache()
+def get_data(lb, hd):
+    lb, hd = map(int, (lb, hd))
+    monthly_rets = MONTHLY_RETS[(lb, hd)]
+    data = pd.DataFrame(monthly_rets)
+    data.index.name='date'
+    data.sort_index(inplace=True)
+    data['w_netval'] = (data['w'] + 1).cumprod()
+    data['l_netval'] = (data['l'] + 1).cumprod()
+    data['wl_netval'] = (data['wl'] + 1).cumprod()
+    data['w_drawdown'] = (data['w_netval'] / data['w_netval'].cummax()) - 1
+    data['l_drawdown'] = (data['l_netval'] / data['l_netval'].cummax()) - 1
+    data['wl_drawdown'] = (data['wl_netval'] / data['wl_netval'].cummax()) - 1
+    return data
+
+
+title = PreText(text='52 Week High', width=500)
+
+# set up widgets
+
+stats = PreText(text='', width=500)
+lookback = Select(value=LOOK_BACK[0], options=LOOK_BACK)
+holding = Select(value=HOLDING[0], options=HOLDING)
+
+# set up plots
+
+source = ColumnDataSource(data=dict(date=[], w_netval=[], l_netval=[], wl_netval=[]))
+tools = 'pan,wheel_zoom,xbox_select,reset'
+
+# ts1
+ts1 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts1.line('date', 'w_netval', source=source)
+ts1.yaxis.axis_label = 'Net Value'
+ts1.circle('date', 'w_netval', size=1, source=source, color=None, selection_color="orange")
+
+# ts2
+ts2 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts2.line('date', 'l_netval', source=source)
+ts2.yaxis.axis_label = 'Net Value'
+ts2.circle('date', 'l_netval', size=1, source=source, color=None, selection_color="orange")
+
+# ts3
+ts3 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts3.line('date', 'wl_netval', source=source)
+ts3.yaxis.axis_label = 'Net Value'
+ts3.circle('date', 'wl_netval', size=1, source=source, color=None, selection_color="orange")
+
+ts2.x_range = ts1.x_range
+ts3.x_range = ts1.x_range
+
+# set up callbacks
+
+def lookback_change(attrname, old, new):
+    update()
+
+def holding_change(attrname, old, new):
+    update()
+
+def update(selected=None):
+    lb, hd = lookback.value, holding.value
+
+    df = get_data(lb, hd)
+    data = df[['w_netval', 'l_netval', 'wl_netval']]
+    source.data = data
+
+    update_stats(df)
+
+    ts1.title.text, ts2.title.text, ts3.title.text = 'Winner', 'Loser', 'Winner-Loser'
+
+def update_stats(data):
+    df = pd.DataFrame(columns=['w', 'l', 'wl'])
+    rets = data[['w', 'l', 'wl']]
+    df.loc['avg monthly ret'] = rets.mean(axis=0)
+    df.loc['t-statistic'] = rets.apply(lambda x: sci_stats.ttest_1samp(x, 0).statistic, axis=0)
+    df.loc['sharpe ratio'] = df.loc['avg monthly ret'] / rets.std(axis=0) * np.sqrt(12)
+    for col in df.columns:
+        df.loc['max drawdown', col] = data[f'{col}_drawdown'].min()
+    stats.text = str(df)
+
+lookback.on_change('value', lookback_change)
+holding.on_change('value', holding_change)
+
+def selection_change(attrname, old, new):
+    lb, hd = lookback.value, holding.value
+
+    data = get_data(lb, hd)
+    selected = source.selected.indices
+    if selected:
+        data = data.iloc[selected, :]
+    update_stats(data)
+
+source.selected.on_change('indices', selection_change)
+
+# set up layout
+widgets = column(lookback, holding, stats)
+main_row = row(widgets)
+series = column(ts1, ts2, ts3)
+layout = column(title, main_row, series)
+
+# initialize
+update()
+
+curdoc().add_root(layout)
+curdoc().title = "Stocks"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+from bokeh.io import output_file, show
+from bokeh.palettes import Spectral5
+from bokeh.plotting import figure
+from bokeh.sampledata.autompg import autompg as df
+from bokeh.transform import factor_cmap
+
+df.cyl = df.cyl.astype(str)
+group = df.groupby('cyl')
+
+cyl_cmap = factor_cmap('cyl', palette=Spectral5, factors=sorted(df.cyl.unique()))
+
+p12 = figure(plot_height=350, x_range=group, title="MPG by # Cylinders",
+           toolbar_location=None, tools="")
+
+p12.vbar(x='cyl', top='mpg_mean', width=1, source=group,
+       line_color=cyl_cmap, fill_color=cyl_cmap)
+
+p12.y_range.start = 0
+p12.xgrid.grid_line_color = None
+p12.xaxis.axis_label = "some stuff"
+p12.xaxis.major_label_orientation = 1.2
+p12.outline_line_color = None
+"""
+
+
+
+
+
+
+
+
