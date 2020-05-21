092a418c5fc3a7ebe379371caacd0d7966b70c77
Zhishe Chen <33152781+JakeChen6@users.noreply.github.com>
2020-05-21 12:25:30 +0800

    Update 52_week_high

--- 52_week_high/calc_signals.py
+++ 52_week_high/calc_signals.py
@@ -1,190 +1,177 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+
+
 import os
 import time
-import datetime as dt
 import pickle as pk
-from functools import reduce
 from concurrent import futures
 
-import psutil
 import numpy as np
 import pandas as pd
 
-ENV_PATH = '/Users/zhishe/myProjects/anomaly'
+DIR = '/Users/zhishe/myProjects/anomaly'
+
+
+#%%
+
+
+# anomaly setting
 
 NAME = '52_week_high'
 
-
-#%%
-
-# Filter
-
-# common stocks only
-#COMMON_STOCKS = [10, 11]
-
-# exclude penny stocks
-PRICE_LIMIT = 5
-
-# listed on NYSE, AMEX, or NASDAQ
-EXCH_CODE = [1, 2, 3]
+START = 1963
+END = 2018
+LOOK_BACK = [12]
+HOLDING = [6]
 
 
 #%%
 
-# Read data
 
-# monthly stock data
-MSF = pd.read_hdf(ENV_PATH + '/data/msf.h5', key='msf')
+# constraints
 
-# common stocks - PERMNOs
-with open(ENV_PATH + '/data/common_stock_permno.pkl', 'rb') as f:
+"""
+1963 - 2001
+NYSE, AMEX, NASDAQ
+Common stocks
+Exclude if price < $5
+"""
+
+EXCH_CODE = [1, 2, 3]  # NYSE, AMEX, NASDAQ
+COMMON_STOCK_CD = [10, 11]  # only common stocks
+PRICE_LIMIT = 5. # Exclude if price < $5
+
+
+#%%
+
+
+# read data
+
+MSF = pd.read_hdf(DIR + '/data/msf.h5', key='msf')
+MSF.PRC /= MSF.CFACPR  # adjust prices
+MSF.ASKHI /= MSF.CFACPR  # adjust prices
+
+with open(DIR + '/data/common_stock_permno.pkl', 'rb') as f:
     COMMON_STOCK_PERMNO = pk.load(f)
 
-# range of dates
-DATE_RANGE = MSF['DATE'].unique()
+DATE_RANGE = MSF.DATE.unique()
 DATE_RANGE.sort()
 
 
 #%%
 
-# Apply filter
 
-# exclude penny stocks
-MSF = MSF[MSF['PRC'].abs() >= PRICE_LIMIT].copy()
+# signal calculation algorithm
 
-# listed on NYSE, AMEX, or NASDAQ
-conds = (MSF['HEXCD'] == c for c in EXCH_CODE)
-cond = reduce(lambda x, y: x | y, conds)
-MSF = MSF[cond].copy()
+# ratio of current price to the highest price during the past 12 months
 
-
-#%%
-
-# Define signal calculation process
-
-# signal: ratio of current price to the highest price during the past 12 months.
-
-# adjust prices
-MSF['PRC'] = MSF['PRC'].abs() / MSF['CFACPR']
-MSF['ASKHI'] = MSF['ASKHI'].abs() / MSF['CFACPR']
-
-
-def calc_ratios(m):
+def calc_ratio(m, lb):
     """
-    Calculate for each stock its ratio of the current price to the highest price
-    during the past 12 months.
-
-    Parameters
-    ----------
-    m : np.datetime64
-        current month
-
-    Returns
-    -------
-    pd.Series
-
+    m: np.datetime64
+    lb: int
     """
-    # SELECT data
-    start, end = DATE_RANGE[DATE_RANGE < m][[-12, -1]]  # data of past 12 months
-    data = MSF[
-        (MSF['DATE'] >= start) &
-        (MSF['DATE'] <= end)
-    ]
+    # past lb months
+    start, end = DATE_RANGE[DATE_RANGE < m][[-lb, -1]]
 
-    # only include common stocks
-    common_stock_permno = COMMON_STOCK_PERMNO[end]  
+    data = MSF[MSF.DATE == end]  # data of month m-1
+    # apply constraints
+    data = data[data.HEXCD.isin(EXCH_CODE)]  # exchange constraint
+    data = data[data.PRC.abs() >= PRICE_LIMIT]  # price constraint
 
-    data = data.set_index('PERMNO')
-    index = set(data.index) & set(common_stock_permno)
-    data = data.loc[index]  # only keep common stocks
-    
-    # calculate each stock's ratio
-    ratios = {}
-    for p in data.index.unique():
-        subdata = data[data.index == p]
-        if subdata.shape[0] < 10:  # less than 10 months' data available
-            continue
-        subdata = subdata.set_index('DATE').sort_index()
-        if subdata.index[-1] != end:  # no data on month m-1
-            continue
-        highest = subdata['ASKHI'].max()
-        r = subdata.iloc[-1]['PRC'] / highest
-        if pd.notna(r):
-            ratios[p] = r
+    common_stock_permno = COMMON_STOCK_PERMNO[end]  # PERMNO of common stocks according to information on 'end'
+    data = data[data.PERMNO.isin(common_stock_permno)]  # common stock constraint
 
-    # save in a pandas.Series
-    series = pd.Series(ratios)
-    series.name = 'RATIO'
-    series.index.name = 'PERMNO'
+    # get data in the past lb months for the eligible stocks
+    eligible_permno = data.PERMNO.values
+    data = MSF[(MSF.DATE >= start) & (MSF.DATE <= end) & (MSF.PERMNO.isin(eligible_permno))]
 
-    return series
+    # ratio of current price to the highest price during the past 12 months
+
+    def helper_calc_ratio(df):
+        """
+        df: pandas.DataFrame
+        """
+        df = df.dropna(subset=['PRC'])
+        df.set_index('DATE', inplace=True)
+        
+        if end not in df.index:  # no data on month m-1
+            return np.nan
+
+        highest = df.ASKHI.abs().max()
+        if pd.isna(highest) or highest == 0.:
+            return np.nan
+
+        r = abs(df.loc[end, 'PRC']) / highest  # current price scaled by the highest price
+        return r
+
+    ratios = data.groupby('PERMNO').apply(helper_calc_ratio)
+    ratios.dropna(inplace=True)
+
+    ratios.name = 'RATIO'
+
+    return ratios
 
 
-def get_signals(args):
-    """
-    pno:            process identifier
-    subrange:       subset of the date_range
-    lb:             look back period
-    """
-    subrange, lb = args
-
-    signals = {m: calc_ratios(m) for m in subrange}
-
+def calc_signals(args):
+    sub_range, lb = args
+    signals = {m: calc_ratio(m, lb) for m in sub_range}
     return signals
 
 
 #%%
 
-CPU_COUNT = psutil.cpu_count(logical=False)
 
-COLLECTOR = {}  # the container where we store the computation results
+# distribute computations to multiple CPUs
 
-START = 1963
-END   = 2018
+CPU_COUNT = 8
 
-# look back periods & holding periods
-LOOK_BACK = [12]
-HOLDING   = [6]
+start = DATE_RANGE[DATE_RANGE >= np.datetime64(f'{START}-01-01')][0]
+end = DATE_RANGE[DATE_RANGE <= np.datetime64(f'{END}-12-31')][-1]
 
-START = DATE_RANGE[DATE_RANGE >= np.datetime64(dt.date(START, 1, 1))][0]
-END = DATE_RANGE[DATE_RANGE <= np.datetime64(dt.date(END, 12, 31))][-1]
-
+collector = {}
 
 for lb in LOOK_BACK:
     hd = max(HOLDING)
-    print('\nCalculating (%s, %s) strategy...' % (lb, hd), end='\t')
+    print(f'\nCalculating ({lb}, {hd}) strategy...', end='\t')
 
     # on this date we calculate the first set of signals
-    first_date = DATE_RANGE[DATE_RANGE <= START][-hd]
+    first_date = DATE_RANGE[DATE_RANGE <= start][-hd]
     # calculate signals for every month in this range
-    date_range = DATE_RANGE[(DATE_RANGE >= first_date) & (DATE_RANGE <= END)]
-
-    # we will split the task and distribute the workloads to multiple processes
-    size = len(date_range) // CPU_COUNT  # the size of each workload
-    chunks = []
+    date_range = DATE_RANGE[(DATE_RANGE >= first_date) & (DATE_RANGE <= end)]
+    
+    # split the workload
+    size = len(date_range) // CPU_COUNT
+    sub_ranges = []
     for i in range(CPU_COUNT):
         if i != CPU_COUNT-1:
-            chunks.append(date_range[size*i:size*(i+1)])
+            sub_ranges.append(date_range[size*i:size*(i+1)])
         else:
-            chunks.append(date_range[size*i:])
+            sub_ranges.append(date_range[size*i:])
 
     with futures.ProcessPoolExecutor(max_workers=CPU_COUNT) as ex:
-        start_time = time.time()
-        res = ex.map(get_signals, zip(chunks, [lb] * CPU_COUNT))
+        ts = time.time()
+        res = ex.map(calc_signals, zip(sub_ranges, [lb] * CPU_COUNT))
         for signals in res:
-            COLLECTOR.setdefault(lb, {}).update(signals)
-        print('{:.2f} s.'.format(time.time() - start_time))
+            collector.setdefault(lb, {}).update(signals)
+        te = time.time()
+        print('{:.2f}s'.format(te - ts))
 
 
 #%%
-# Output
 
-if not os.path.exists(ENV_PATH + f'/results/{NAME}'):
-    os.mkdir(ENV_PATH + f'/results/{NAME}')
-    os.mkdir(ENV_PATH + f'/results/{NAME}/signals')
+
+# save results to local
+
+if not os.path.exists(DIR + f'/results/{NAME}'):
+    os.mkdir(DIR + f'/results/{NAME}')
+    os.mkdir(DIR + f'/results/{NAME}/signals')
 
 for lb in LOOK_BACK:
     table = pd.DataFrame()
-    signals = COLLECTOR[lb]
+    signals = collector[lb]
     # consolidate each month's signals into a single table
     for k, v in signals.items():
         df = pd.DataFrame(v)
@@ -192,8 +179,8 @@ for lb in LOOK_BACK:
         df['DATE'] = k
         table = pd.concat([table, df], ignore_index=True)
 
-    table.rename(columns={'RATIO': 'SIGNAL'}, inplace=True)
-    table = table.reindex(columns=['DATE', 'PERMNO', 'SIGNAL'])
+    table = table.reindex(columns=['DATE', 'PERMNO', 'RATIO'])
     table.sort_values(by='DATE', inplace=True)
-    table.to_csv(ENV_PATH + f'/results/{NAME}/signals/{lb}.csv')
-    print('%s done.' % lb)
+    table.to_csv(DIR + f'/results/{NAME}/signals/{lb}.csv')
+    print(f'{lb} done.')
+
