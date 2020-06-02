4b75ad35036256c4ba221b3f29be00aa2da609e0
Zhishe Chen <33152781+JakeChen6@users.noreply.github.com>
2020-06-02 15:07:06 +0800

    Create seasonality

---
+++ seasonality/calc_signals.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+
+
+import os
+import time
+import pickle as pk
+from concurrent import futures
+
+import numpy as np
+import pandas as pd
+
+DIR = '/Users/zhishe/myProjects/anomaly'
+
+
+#%%
+
+# read data
+
+MSF = pd.read_hdf(DIR + '/data/msf.h5', key='msf')
+
+with open(DIR + '/data/common_stock_permno.pkl', 'rb') as f:
+    COMMON_STOCK_PERMNO = pk.load(f)
+
+DATE_RANGE = MSF.DATE.unique()
+DATE_RANGE.sort()
+
+
+#%%
+
+
+# anomaly setting
+
+NAME = 'seasonality'
+
+START = 1965
+END = 2018
+LOOK_BACK = [12*20]  # 20 years
+HOLDING = [1]
+
+
+#%%
+
+
+# constraints
+
+"""
+January 1965 - December 2002
+NYSE, AMEX
+Common stocks
+Exclude if price < $5
+At least one year of data to be included in the sample
+"""
+START = DATE_RANGE[DATE_RANGE >= np.datetime64(f'{START}-01-01')][0]
+END = DATE_RANGE[DATE_RANGE <= np.datetime64(f'{END}-12-31')][-1]
+
+EXCH_CODE = [1, 2]  # NYSE, AMEX
+COMMON_STOCK_CD = [10, 11]  # only common stocks
+PRICE_LIMIT = 5. # Exclude if price < $5
+HISTORY_LIMIT = 12  # At least one year of data to be included in the sample
+
+
+#%%
+
+
+# signal calculation algorithm
+
+# average monthly return in the same month over the last 20 years.
+
+
+def calc_avg_monthly_return(month, lb):
+    """
+    Use information before 'month' (including 'month') to calculate the signals.
+
+    month : np.datetime64
+    lb : int
+    """
+    next_month = DATE_RANGE[DATE_RANGE > month][0]  # next month
+    start = DATE_RANGE[DATE_RANGE < next_month][-lb]  # 20 years ago
+    cycle = DATE_RANGE[  # the same months in the last 20 years
+        (DATE_RANGE >= start) &
+        (DATE_RANGE < next_month)
+        ][::12]
+
+    data = MSF[MSF.DATE == month]
+    # apply constraints
+    data = data[data.HEXCD.isin(EXCH_CODE)]  # exchange constraint
+    data = data[data.PRC.abs() >= PRICE_LIMIT]  # price constraint
+
+    common_stock_permno = COMMON_STOCK_PERMNO[month]  # PERMNO of common stocks
+    data = data[data.PERMNO.isin(common_stock_permno)]  # common stock constraint
+
+    start = DATE_RANGE[DATE_RANGE < next_month][-HISTORY_LIMIT]
+    past_data = MSF[
+        (MSF.PERMNO.isin(data.PERMNO.values)) &
+        (MSF.DATE >= start) &
+        (MSF.DATE <= month)
+        ]
+    ret_data_months = past_data.groupby('PERMNO').RET.count()  # missing values not included in count
+    eligible = ret_data_months[ret_data_months == HISTORY_LIMIT].index  # history constraint
+
+    # get data in the same months in the last 20 years for the eligible stocks
+    data = MSF[
+        (MSF.PERMNO.isin(eligible)) &
+        (MSF.DATE.isin(cycle))
+        ]
+    
+    # calculate signals
+    avg_monthly_rets = data.groupby('PERMNO').RET.mean()
+    avg_monthly_rets.dropna(inplace=True)
+
+    return avg_monthly_rets
+
+
+def calc_signals(args):
+    sub_range, lb = args
+    signals = {m: calc_avg_monthly_return(m, lb) for m in sub_range}
+    return signals
+
+
+#%%
+
+
+# distribute computations to multiple CPUs
+
+CPU_COUNT = 8
+
+collector = {}
+
+for lb in LOOK_BACK:
+    hd = max(HOLDING)
+    print(f'\nCalculating ({lb}, {hd}) strategy...', end='\t')
+
+    # on this date we calculate the first set of signals
+    first_date = DATE_RANGE[DATE_RANGE < START][-hd]
+    # on this date we calculate the last set of signals
+    last_date = DATE_RANGE[DATE_RANGE < END][-1]
+    # calculate signals for every month in this range
+    date_range = DATE_RANGE[
+        (DATE_RANGE >= first_date) &
+        (DATE_RANGE <= last_date)
+        ]
+    
+    # split the workload
+    size = len(date_range) // CPU_COUNT
+    sub_ranges = []
+    for i in range(CPU_COUNT):
+        if i != CPU_COUNT-1:
+            sub_ranges.append(date_range[size*i:size*(i+1)])
+        else:
+            sub_ranges.append(date_range[size*i:])
+
+    with futures.ProcessPoolExecutor(max_workers=CPU_COUNT) as ex:
+        ts = time.time()
+        res = ex.map(calc_signals, zip(sub_ranges, [lb] * CPU_COUNT))
+        for signals in res:
+            collector.setdefault(lb, {}).update(signals)
+        te = time.time()
+        print('{:.2f}s'.format(te - ts))
+
+
+#%%
+
+
+# save results to local
+
+path = DIR + f'/anomaly-project/{NAME}/signals'
+if not os.path.exists(path):
+    os.mkdir(path)
+
+for lb in LOOK_BACK:
+    table = pd.DataFrame()
+    signals = collector[lb]
+    # consolidate each month's signals into a single table
+    for k, v in signals.items():
+        df = pd.DataFrame(v)
+        df.reset_index(inplace=True)
+        df['DATE'] = k
+        table = pd.concat([table, df], ignore_index=True)
+
+    table = table.reindex(columns=['DATE', 'PERMNO', 'RET'])
+    table.sort_values(by='DATE', inplace=True)
+    table.to_csv(path + f'/{lb}.csv')
+    print(f'{lb} done.')
+
---
+++ seasonality/plot/main.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+
+
+from functools import lru_cache
+import pickle as pk
+
+import numpy as np
+import pandas as pd
+from scipy import stats as sci_stats
+
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, PreText, Select, TextInput, Button
+from bokeh.plotting import figure
+
+DIR = '/Users/zhishe/myProjects/anomaly'
+
+
+#%%
+
+
+# anomaly setting
+
+NAME = 'seasonality'
+
+LOOK_BACK = ['240']
+HOLDING = ['1']
+
+
+#%%
+
+
+# read data
+
+with open(DIR + f'/anomaly-project/{NAME}/returns/monthly_returns.pkl', 'rb') as f:
+    MONTHLY_RETS = pk.load(f)
+
+
+#%%
+
+
+@lru_cache()
+def get_data(lb, hd):
+    lb, hd = map(int, (lb, hd))
+    monthly_rets = MONTHLY_RETS[(lb, hd)]
+    data = pd.DataFrame(monthly_rets)
+    data.index.name='date'
+    data.sort_index(inplace=True)
+    return data
+
+
+def calc_netval_and_drawdown(data):
+    data = data.copy()
+    data['w_netval'] = (data['w'] + 1).cumprod()
+    data['l_netval'] = (data['l'] + 1).cumprod()
+    data['ls_netval'] = (data['ls'] + 1).cumprod()
+    data['w_drawdown'] = (data['w_netval'] / data['w_netval'].cummax()) - 1
+    data['l_drawdown'] = (data['l_netval'] / data['l_netval'].cummax()) - 1
+    data['ls_drawdown'] = (data['ls_netval'] / data['ls_netval'].cummax()) - 1
+    return data
+
+
+#%%
+
+
+title = PreText(text=NAME, width=500)
+
+# set up widgets
+stats = PreText(text='', width=500)  # the statistics table
+lookback = Select(value=LOOK_BACK[0], options=LOOK_BACK)  # length of look back period
+holding = Select(value=HOLDING[0], options=HOLDING)  # length of holding period
+start = TextInput(value='', title='Start')  # start of sample
+end = TextInput(value='', title='End')  # end of sample
+button = Button(label='Change', button_type='primary')
+
+# set up plots
+source = ColumnDataSource(data=dict(date=[], w_netval=[], l_netval=[], ls_netval=[]))
+tools = 'pan,wheel_zoom,xbox_select,reset'
+
+# ts1
+ts1 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts1.line('date', 'w_netval', source=source)
+ts1.yaxis.axis_label = 'Net Value'
+ts1.circle('date', 'w_netval', size=1, source=source, color=None, selection_color="orange")
+
+# ts2
+ts2 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts2.line('date', 'l_netval', source=source)
+ts2.yaxis.axis_label = 'Net Value'
+ts2.circle('date', 'l_netval', size=1, source=source, color=None, selection_color="orange")
+
+# ts3
+ts3 = figure(plot_width=900, plot_height=200, tools=tools, x_axis_type='datetime', active_drag="xbox_select")
+ts3.line('date', 'ls_netval', source=source)
+ts3.yaxis.axis_label = 'Net Value'
+ts3.circle('date', 'ls_netval', size=1, source=source, color=None, selection_color="orange")
+
+ts1.title.text, ts2.title.text, ts3.title.text = 'Winner', 'Loser', 'Long-Short'
+
+ts2.x_range = ts1.x_range
+ts3.x_range = ts1.x_range
+
+
+#%%
+
+
+# set up callbacks
+
+def lookback_change(attrname, old, new):
+    start.value = ''  # refresh
+    end.value = ''
+    update()
+
+def holding_change(attrname, old, new):
+    start.value = ''  # refresh
+    end.value = ''
+    update()
+
+def button_click():
+    update(s=start.value, e=end.value)
+
+
+def update(s=None, e=None):
+    lb, hd = lookback.value, holding.value
+    data = get_data(lb, hd)
+
+    # assuming valid inputs below, i.e., both s and e are in range.
+    if s:
+        s = pd.Timestamp(s)
+        data = data.loc[s:]
+    if e:
+        e = pd.Timestamp(e)
+        data = data.loc[:e]
+
+    data = calc_netval_and_drawdown(data)  # calculate net value and drawdown
+    source.data = data[['w_netval', 'l_netval', 'ls_netval']]
+
+    update_stats(data)
+
+
+def update_stats(data):
+    df = pd.DataFrame(columns=['w', 'l', 'ls'])
+    rets = data[['w', 'l', 'ls']]
+    df.loc['avg monthly ret'] = rets.mean(axis=0)
+    df.loc['t-statistic'] = rets.apply(lambda x: sci_stats.ttest_1samp(x, 0).statistic, axis=0)
+    df.loc['skew'] = rets.apply(lambda x: x.skew(), axis=0)  # skewness
+    df.loc['kurtosis'] = rets.apply(lambda x: x.kurtosis(), axis=0)  # kurtosis
+    df.loc['sharpe ratio'] = df.loc['avg monthly ret'] / rets.std(axis=0) * np.sqrt(12)  # scale to annual
+    # calculate positive month %
+    for col in df.columns:
+        s = rets[col]
+        df.loc['positive month %', col] = s[s > 0].shape[0] / s.shape[0]
+    # calculate maximum drawdown
+    for col in df.columns:
+        df.loc['max drawdown', col] = data[f'{col}_drawdown'].min()
+    
+    stats.text = str(df)
+
+
+def selection_change(attrname, old, new):
+    lb, hd = lookback.value, holding.value
+    data = get_data(lb, hd)
+    selected = source.selected.indices
+    if selected:
+        data = data.iloc[selected, :]
+        start.value = str(data.index[0].date())
+        end.value = str(data.index[-1].date())
+    data = calc_netval_and_drawdown(data)  # calculate net value and drawdown
+    update_stats(data)
+
+
+lookback.on_change('value', lookback_change)
+holding.on_change('value', holding_change)
+button.on_click(button_click)
+
+source.selected.on_change('indices', selection_change)
+
+
+#%%
+
+
+# set up layout
+widgets = column(lookback, holding, row(start, end), button, stats)
+main_row = row(widgets)
+series = column(ts1, ts2, ts3)
+layout = column(title, main_row, series)
+
+# initialize
+update()
+
+curdoc().add_root(layout)
+curdoc().title = NAME
+
