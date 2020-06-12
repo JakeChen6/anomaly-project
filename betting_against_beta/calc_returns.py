a45aba33013c093831a59880ac63fa8d3fb66771
Zhishe Chen <33152781+JakeChen6@users.noreply.github.com>
2020-06-12 11:29:12 +0800

    Create betting_against_beta

---
+++ betting_against_beta/calc_returns.py
@@ -0,0 +1,217 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+
+
+import os
+import time
+import pickle as pk
+from concurrent import futures
+
+import numpy as np
+import pandas as pd
+
+DIR = '/Users/zhishe/myProjects/anomaly'
+
+
+#%%
+
+
+# read data
+
+MSF = pd.read_hdf(DIR + '/data/msf.h5', key='msf')
+
+DATE_RANGE = MSF.DATE.unique()
+DATE_RANGE.sort()
+
+
+#%%
+
+
+# anomaly setting
+
+NAME = 'betting_against_beta'
+
+START = 1932
+END = 2018
+LOOK_BACK = [60]
+HOLDING = [1, 6, 12]
+
+
+START = DATE_RANGE[DATE_RANGE >= np.datetime64(f'{START}-01-01')][0]
+
+
+#%%
+
+
+# return calculation algorithm
+
+# given signals, calculate portfolio series;
+# given portfolio series, calculate monthly return series;
+# given monthly return series, display plots and statistics.
+
+def calc_portfolios(lb):
+    """
+    Given signals from a CSV file, calculate a winner portfolio series
+    and a loser portfolio series.
+    
+    lb: int
+    """
+    signals = pd.read_csv(DIR + f'/anomaly-project/{NAME}/signals/{lb}.csv')
+    signals['DATE'] = pd.to_datetime(signals['DATE'])  # str to datetime
+
+    winners = {}
+    losers = {}
+    
+    # in each month we have a new winner portfolio and a new loser portfolio
+    # computed based on signal ranking.
+    for month in signals.DATE.unique():
+        rows = signals[signals.DATE == month]
+        rows = rows.set_index('PERMNO')
+        beta = rows.BETA.sort_values()
+        num_in_decile = beta.size // 10  # number of stocks in a decile
+        winner_threshold = beta.iloc[-num_in_decile]  # threshold of bottom decile 
+        loser_threshold = beta.iloc[num_in_decile-1]  # threshold of top decile
+        winners[month] = beta[beta >= winner_threshold].index.tolist()
+        losers[month] = beta[beta <= loser_threshold].index.tolist()
+
+    return winners, losers
+
+
+def calc_port_ret(portfolio, data):
+    """
+    Calculate the portfolio's return in the month.
+    
+    portfolio: list
+        a list of PERMNO representing the equally weighted portfolio
+    """
+    if not portfolio:
+        return 0
+
+    permnos = data.index.intersection(portfolio)
+    ret = data.loc[permnos, 'RET'].sum() / len(portfolio)
+
+    return ret
+
+
+def calc_monthly_rets(*args):
+    """
+    Given winners and losers, calculate their monthly return series
+    and the monthly return series of the corresponding long-short portfolio.
+    
+    portfolios: tuple
+        (winners, losers)
+    hd: int
+    """
+    portfolios, hd = args
+    winners, losers = portfolios
+
+    monthly_rets = {
+        'w': {},
+        'l': {},
+        'ls': {}
+        }
+
+    sorted_months = np.array(sorted(winners.keys()))
+
+    for current_month in DATE_RANGE:
+        if current_month < START:
+            continue
+
+        data = MSF[(MSF.DATE == current_month) & (MSF.RET.notna())]
+        data = data.set_index('PERMNO')
+
+        # the monthly return in each month is calculated as the
+        # equally weighted average of the returns from the hd separate portfolios.
+        current_month_rets = {
+            'w': 0,
+            'l': 0,
+            'ls': 0
+            }
+
+        for previous_month in sorted_months[sorted_months < current_month][-hd:]:
+            w = winners[previous_month]  # the winner portfolio
+            l = losers[previous_month]  # the loser portfolio
+            wret = calc_port_ret(w, data)  # winner's return in the current month
+            lret = calc_port_ret(l, data)  # loser's return in the current month
+            current_month_rets['w'] += wret
+            current_month_rets['l'] += lret
+            current_month_rets['ls'] += lret - wret  # long loser, short winner
+
+        # the equally weighted average
+        monthly_rets['w'][current_month] = current_month_rets['w'] / hd
+        monthly_rets['l'][current_month] = current_month_rets['l'] / hd
+        monthly_rets['ls'][current_month] = current_month_rets['ls'] / hd
+
+    return monthly_rets
+
+
+#%%
+
+
+def helper_timeit(func, *args):
+    ts = time.time()
+    res = func(*args)
+    te = time.time()
+    return res, te - ts
+
+
+#%%
+
+
+# calculate portfolios
+
+if not os.path.exists(DIR + f'/anomaly-project/{NAME}/returns'):
+    os.mkdir(DIR + f'/anomaly-project/{NAME}/returns')
+
+collector_portfolios = {}
+
+with futures.ProcessPoolExecutor(max_workers=4) as ex:
+    wait_for = []
+    for lb in LOOK_BACK:
+        print(f'Calculating look back {lb} portfolios...')
+        f = ex.submit(helper_timeit, calc_portfolios, lb)  # schedule the future to run
+        f.arg = lb
+        wait_for.append(f)
+    
+    # collect results returned from processes
+    for f in futures.as_completed(wait_for):
+        lb = f.arg
+        res, t = f.result()
+        collector_portfolios[lb] = res
+        print('look back {} done, {:.2f}s.'.format(lb, t))
+
+# save to local
+with open(DIR + f'/anomaly-project/{NAME}/returns/portfolios.pkl', 'wb') as f:
+    pk.dump(collector_portfolios, f)
+
+
+#%%
+
+
+# calculate monthly returns
+
+collector_monthly_rets = {}
+
+with futures.ProcessPoolExecutor(max_workers=4) as ex:
+    wait_for = []
+    for lb in LOOK_BACK:
+        for hd in HOLDING:
+            print(f'Calculating {lb}-{hd} monthly returns...')
+            winners, losers = collector_portfolios[lb]
+            f = ex.submit(helper_timeit, calc_monthly_rets, (winners, losers), hd)  # schedule the future to run
+            f.arg = (lb, hd)
+            wait_for.append(f)
+
+    # collect results returned from processes
+    for f in futures.as_completed(wait_for):
+        key = f.arg
+        res, t = f.result()
+        collector_monthly_rets[key] = res
+        print('{}-{} done, {:.2f}s.'.format(*key, t))
+
+# save to local
+with open(DIR + f'/anomaly-project/{NAME}/returns/monthly_returns.pkl', 'wb') as f:
+    pk.dump(collector_monthly_rets, f)
+
---
+++ betting_against_beta/calc_signals.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#%%
+
+
+import os
+import time
+import pickle as pk
+from concurrent import futures
+
+import numpy as np
+import pandas as pd
+import vaex
+import statsmodels.api as sm
+from scipy.stats import pearsonr
+
+DIR = '/Users/zhishe/myProjects/anomaly'
+
+
+#%%
+
+
+# read data
+
+DSF = vaex.open(DIR + '/data/Stock/h5/dsf.h5')
+
+with open(DIR + '/data/common_stock_permno.pkl', 'rb') as f:
+    COMMON_STOCK_PERMNO = pk.load(f)
+
+MSF = pd.read_hdf(DIR + '/data/msf.h5', key='msf')
+DATE_RANGE = MSF.DATE.unique()
+DATE_RANGE.sort()
+
+DSI = pd.read_csv(DIR + '/data/dsi.csv', index_col=0, parse_dates=True)
+
+
+#%%
+
+
+# anomaly setting
+
+NAME = 'betting_against_beta'
+
+START = 1932
+END = 2018
+LOOK_BACK = [60]
+HOLDING = [1, 6, 12]
+
+
+#%%
+
+
+# constraints
+
+"""
+July 1926 - March 2012
+NYSE, AMEX, NASDAQ
+Common stocks
+Exclude if price < $5
+at least 120 daily returns in the one-year period
+at least 750 daily returns in the five-year period
+"""
+START = DATE_RANGE[DATE_RANGE >= np.datetime64(f'{START}-01-01')][0]
+END = DATE_RANGE[DATE_RANGE <= np.datetime64(f'{END}-12-31')][-1]
+
+EXCH_CODE = [1, 2, 3]  # NYSE, AMEX, NASDAQ
+COMMON_STOCK_CD = [10, 11]  # only common stocks
+PRICE_LIMIT = 5. # Exclude if price < $5
+HISTORY_LIMIT_SIGMA = 120  # at least 120 daily returns in the one-year period
+HISTORY_LIMIT_ROU = 750  # at least 750 daily returns in the five-year period
+
+
+#%%
+
+
+# signal calculation algorithm
+
+# beta: rou * firm sigma / market sigma, sigmas are calculated as the standard
+# deviation of daily log returns over a 1-year rolling window (with at least
+# 120 daily returns); correlation is calculated using overlapping 3-day log
+# returns over a 5-year rolling window (with at least 750 daily returns).
+
+MONTHBEGIN = pd.tseries.offsets.MonthBegin()
+
+CPU_COUNT = 6
+
+
+def calc_beta(month, lb):
+    """
+    Use information before 'month' (including 'month') to calculate the signals.
+
+    month: np.datetime64
+    lb: int
+    """
+    ts = time.time()
+
+    data = MSF[MSF.DATE == month]
+    # apply constraints
+    data = data[data.HEXCD.isin(EXCH_CODE)]  # exchange constraint
+    data = data[data.PRC.abs() >= PRICE_LIMIT]  # price constraint
+
+    common_stock_permno = COMMON_STOCK_PERMNO[month]  # PERMNO of common stocks according to information on 'end'
+    data = data[data.PERMNO.isin(common_stock_permno)]  # common stock constraint
+
+    # one year ago
+    one_year_ago = DATE_RANGE[DATE_RANGE <= month][-12]
+    one_year_ago = MONTHBEGIN.rollback(one_year_ago).to_datetime64()
+    # five years ago
+    five_years_ago = DATE_RANGE[DATE_RANGE <= month][-12*5]
+    five_years_ago = MONTHBEGIN.rollback(five_years_ago).to_datetime64()
+
+    # apply history constraints
+    past_data_5y = DSF[(DSF.PERMNO.isin(data.PERMNO.values)) &  # data in the past 5 year
+                       (DSF.DATE >= five_years_ago) &
+                       (DSF.DATE <= month)].to_pandas_df()
+    rets_count = past_data_5y.groupby('PERMNO').RET.count()  # count non-missing returns
+    eligible = rets_count[rets_count >= HISTORY_LIMIT_ROU].index  # history constraint 1
+
+    past_data_1y = past_data_5y[(past_data_5y.PERMNO.isin(eligible)) &  # data in the past 1 year
+                                (past_data_5y.DATE >= one_year_ago)]
+    rets_count = past_data_1y.groupby('PERMNO').RET.count()  # count non-missing returns
+    eligible = rets_count[rets_count >= HISTORY_LIMIT_SIGMA].index  # history constraint 2
+
+    # get data in the past 5 years for the eligible stocks
+    past_data_5y = past_data_5y[past_data_5y.PERMNO.isin(eligible)]
+    # calculate rou
+    rous = {}
+    mktret = DSI[(DSI.index >= five_years_ago) &
+                 (DSI.index <= month)][['vwretd']].copy()
+    mktret['1'] = mktret.vwretd.shift(-1)
+    mktret['2'] = mktret.vwretd.shift(-2)
+    mktret.dropna(inplace=True)
+    mktret = np.log(mktret + 1).sum(axis=1)  # overlapping 3-day log returns
+
+    past_data_5y = past_data_5y[['PERMNO', 'DATE', 'RET']]
+    workloads = past_data_5y.PERMNO.unique()
+    s = len(workloads) // CPU_COUNT  # load size
+    to_distribute = [workloads[s*i:s*(i+1)] if i != CPU_COUNT-1 else workloads[s*i:]
+                     for i in range(CPU_COUNT)]
+
+    rous = {}
+    with futures.ProcessPoolExecutor(max_workers=CPU_COUNT) as ex:
+        wait_for = []
+        for elm in to_distribute:
+            f = ex.submit(helper_calc_corr, past_data_5y, mktret, elm)
+            wait_for.append(f)
+        for f in futures.as_completed(wait_for):
+            rous.update(f.result())
+
+    # get data in the past 1 year for the eligible stocks
+    past_data_1y = past_data_1y[past_data_1y.PERMNO.isin(eligible)].copy()
+    # calculate sigma
+    past_data_1y['RET'] = np.log(past_data_1y.RET + 1)  # daily log returns
+    firm_sigma = past_data_1y.groupby('PERMNO').RET.std()
+    mktret = DSI[(DSI.index >= one_year_ago) &
+                 (DSI.index <= month)]['vwretd']
+    mkt_sigma = np.log(mktret + 1).std()
+
+    # calculate beta
+    beta = pd.Series(rous) * firm_sigma / mkt_sigma
+    beta.dropna(inplace=True)
+    beta.name = 'BETA'
+    beta.index.name = 'PERMNO'
+
+    te = time.time()
+    print('{}, {:.2f}s'.format(month, te - ts))
+
+    return beta
+
+
+def helper_calc_corr(past_data_5y, mktret, permnos):
+    res = {}
+    for p in permnos:
+        rows = past_data_5y[past_data_5y.PERMNO == p][['DATE', 'RET']].set_index('DATE')
+        rows['1'] = rows.RET.shift(-1)
+        rows['2'] = rows.RET.shift(-2)
+        rows.dropna(inplace=True)
+        rets = np.log(rows + 1).sum(axis=1)  # overlapping 3-day log returns
+        res[p] = pearsonr(rets, mktret.loc[rets.index])[0]
+    return res
+
+
+def calc_signals(args):
+    sub_range, lb = args
+    signals = {m: calc_beta(m, lb) for m in sub_range}
+    return signals
+
+
+#%%
+
+
+# single process
+
+collector = {}
+
+for lb in LOOK_BACK:
+    hd = max(HOLDING)
+    print(f'\nCalculating ({lb}, {hd}) strategy...', end='\t')
+
+    # on this date we calculate the first set of signals
+    first_date = DATE_RANGE[DATE_RANGE < START][-hd]
+    # on this date we calculate the last set of signals
+    last_date = DATE_RANGE[DATE_RANGE < END][-1]
+    # calculate signals for every month in this range
+    date_range = DATE_RANGE[
+        (DATE_RANGE >= first_date) &
+        (DATE_RANGE <= last_date)
+        ]
+
+    signals = calc_signals((date_range, lb))
+    collector.setdefault(lb, {}).update(signals)
+
+
+#%%
+
+
+# save results to local
+
+path = DIR + f'/anomaly-project/{NAME}/signals'
+if not os.path.exists(path):
+    os.mkdir(path)
+
+for lb in LOOK_BACK:
+    table = pd.DataFrame()
+    signals = collector[lb]
+    # consolidate each month's signals into a single table
+    for k, v in signals.items():
+        df = pd.DataFrame(v)
+        df.reset_index(inplace=True)
+        df['DATE'] = k
+        table = pd.concat([table, df], ignore_index=True)
+
+    table = table.reindex(columns=['DATE', 'PERMNO', 'BETA'])
+    table.sort_values(by='DATE', inplace=True)
+    table.to_csv(path + f'/{lb}.csv')
+    print(f'{lb} done.')
+
